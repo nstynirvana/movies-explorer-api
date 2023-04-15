04e8f3f0d25d7d6d490c7480991a70f0fcbd6ccf
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-04-15 11:56:07 +0300

    fix: переписать миддлвару  аутентификации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,14 @@ const AuthError = require('../errors/AuthError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
 
   let payload;
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'super-secret-key');
+    const token = authorization.replace('Bearer ', '');
+
+    payload = jwt.verify(token, NODE_ENV ? JWT_SECRET : 'super-secret-key');
   } catch (err) {
     throw new AuthError('Необходима авторизация');
   }
